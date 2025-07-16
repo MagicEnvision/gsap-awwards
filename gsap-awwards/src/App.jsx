7e839f48eece023e9bfd3395416e1395b293a200
magicenvision <rjrose2003@gmail.com>
2025-07-16 08:48:07 -0400

    started hero

--- gsap-awwards/src/App.jsx
+++ gsap-awwards/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {ScrollTrigger, SplitText} from 'gsap/all';
 import NavBar from './components/NavBar';
+import Hero from './components/Hero';
 
 
 //must register plugins
@@ -10,6 +11,7 @@ const App = () => {
   return (
    <main>
     <NavBar />
+    <Hero />
    </main>
   )
 }
